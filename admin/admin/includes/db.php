eba143bd6c0bfb288c434c56aa4efd30b4155bcb
golammoula287 <golammoula287@gmail.com>
2024-12-31 01:07:35 +0600

    DBMS_Project

---
+++ admin/admin/includes/db.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "online_shopping_system";
+
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>
